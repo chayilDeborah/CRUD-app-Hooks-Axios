2e5313f606032111719d4b5bf4d2d29d9679a6af
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-09-28 14:30:48 +0100

    create home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 const App = () => {
   return (
     <>
-    <div className='text-3xl'>React CRUD Operations using Hooks and Axios</div>
+    <div className='bg-slate-400 h-[100vh]'>
+    <div className='text-3xl text-center pt-10 flex justify-center items-center h-[80vh] '>React CRUD Operations using Hooks and Axios</div>
+    </div>
     </>
   );
 }
