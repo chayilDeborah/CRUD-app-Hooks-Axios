af9cc6f34235a5c9fd7114d299df813237cc00c9
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-09-28 16:30:35 +0100

    create contact form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import CreateContact from './components/CreateContact'
 
 const App = () => {
   return (
     <>
-    <div className='bg-slate-400 h-[100vh]'>
-    <div className='text-3xl text-center pt-10 flex justify-center items-center h-[80vh] '>React CRUD Operations using Hooks and Axios</div>
+    <div className='bg-slate-400'>
+    <div className='text-3xl text-center pt-10 flex justify-center items-center h-[30vh] '>React CRUD Operations using Hooks and Axios</div>
+    <CreateContact />
     </div>
     </>
   );
