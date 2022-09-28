bdae0ad3986cc586656307bd52cb1e6e9493f33d
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-09-28 14:18:27 +0100

    add tailwindCSS to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+    <div className='text-3xl'>React CRUD Operations using Hooks and Axios</div>
+    </>
   );
 }
 
