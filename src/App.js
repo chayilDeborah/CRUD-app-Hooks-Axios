adc9a9b1efbd1371b8271f66a1e06e085ea05704
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-09-28 23:31:09 +0100

    install react routerdom and create ReadContactpage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-
-import './App.css';
-import CreateContact from './components/CreateContact'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import CreateContact from "./components/CreateContact";
 
 const App = () => {
   return (
     <>
-    <div className='bg-slate-400'>
-    <div className='text-3xl text-center pt-10 flex justify-center items-center h-[30vh] '>React CRUD Operations using Hooks and Axios</div>
-    <CreateContact />
-    </div>
+      <Router>
+        <div className="bg-slate-400">
+          <div className="text-3xl text-center pt-10 flex justify-center items-center h-[30vh] ">
+            React CRUD Operations using Hooks and Axios
+          </div>
+          <Routes>
+          <Route exact path="/create" element={<CreateContact />}/>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
