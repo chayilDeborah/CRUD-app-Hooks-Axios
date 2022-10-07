cc539283aad4e36750f75ba3b1ba91ecdff6db94
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-10-07 13:17:10 +0100

    create update page routing

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import CreateContact from "./components/CreateContact";
 import ReadContact from "./components/ReadContact";
+import UpdateContact from "./components/UpdateContact";
 
 const App = () => {
   return (
     <>
       <Router>
-        <div className="bg-slate-400">
+        <div className="bg-slate-400 h-[100vh]">
           <div className="text-3xl text-center pt-10 flex justify-center items-center h-[30vh] ">
             React CRUD Operations using Hooks and Axios
           </div>
           <Routes>
           <Route exact path="/create" element={<CreateContact />}/>
           <Route exact path="/read" element={<ReadContact />} />
+          <Route exact path="/update" element={<UpdateContact />} />
           </Routes>
         </div>
       </Router>
